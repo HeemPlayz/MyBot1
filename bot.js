bd71c38b9f5f4264ea553a7d18f832ac45e8536d
HeemPlayz <42186537+HeemPlayz@users.noreply.github.com>
2018-08-16 23:38:21 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -767,4 +767,24 @@ client.on('message', message => {
     message.channel.send(embed)      
 }})
 
+client.on('message',async message => {
+    if(message.content.startsWith("~restart")) {
+        if(message.author.id !== "323885452207587329") return message.reply('You aren\'t the bot owner.');
+        message.channel.send('**Restarting.**').then(msg => {
+            setTimeout(() => {
+               msg.edit('**Restarting..**');
+            },1000);
+            setTimeout(() => {
+               msg.edit('**Restarting...**');
+            },2000);
+        });
+        console.log(`${message.author.tag} [ ${message.author.id} ] has restarted the bot.`);
+        console.log(`Restarting..`);
+        setTimeout(() => {
+            client.destroy();
+            client.login(process.env.BOT_TOKEN);
+        },3000);
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
